dda0c9f7669d2c3f6b26d69509d09dc4e6fb1487
chsjen492 <109032458+chsjen492@users.noreply.github.com>
2023-07-25 15:43:48 +0900

    [D3] Title: 구구단2, Time: 8 ms, Memory: 13,336 KB -BaekjoonHub

---
+++ SWEA/D3/12221. 구구단2/구구단2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	int A, B;
+	cin>>T;
+
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		cin>>A>>B;
+        if(A<10&&B<10)
+       	 cout<<"#"<<test_case<<" "<<A*B<<"\n";
+        else
+            cout<<"#"<<test_case<<" -1\n";
+		
+
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
