6b6dbb52c1d595698ecf8abbbb66b179e22cc404
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2024-03-10 23:46:14 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int n, int k) {
+	if (k == 0 || k == n) {
+		return 1;
+	}
+	return func(n - 1, k) + func(n - 1, k - 1);
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+	cout << func(n, k);
+
+}
