d80cfa694f9f295bb7e1bbe152702f0ee2148b8f
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2024-06-27 22:50:13 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string cipher, int code) {
+    string answer = "";
+    for(int i=code-1;i<cipher.length();i+=code){
+        answer+=cipher[i];
+    }
+    return answer;
+}
