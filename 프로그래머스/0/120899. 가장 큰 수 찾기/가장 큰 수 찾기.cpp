e4b9eaae4c544cb80399e376bcebe6bfe11fc2e1
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2024-06-29 23:47:00 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array) {
+    vector<int> answer;
+    int max=0;
+    int mIndex=0;
+    for(int i=0;i<array.size();i++){
+        if(array[i]>max){
+            max=array[i];
+            mIndex=i;
+        }
+    }
+    answer.push_back(max);
+    answer.push_back(mIndex);
+    return answer;
+}
