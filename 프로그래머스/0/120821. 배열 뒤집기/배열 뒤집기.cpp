86444a229aa282b7ccbde309bb808607e382a9e3
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2024-08-02 23:52:49 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    reverse(num_list.begin(), num_list.end());
+    answer=num_list;
+    return answer;
+}
