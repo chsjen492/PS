4f37f79b3e593a9071f2d2a04e82a3736256f456
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2024-12-05 11:12:17 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    char c;
+    
+    for(int i=0;i<225;i++){
+        cin>>c;
+        if(c=='w'){
+            cout<<"chunbae";
+            return 0;
+        }
+        if(c=='b'){
+            cout<<"nabi";
+            return 0;
+        }
+        if(c=='g'){
+            cout<<"yeongcheol";
+            return 0;
+        }
+    }
+    
+}
