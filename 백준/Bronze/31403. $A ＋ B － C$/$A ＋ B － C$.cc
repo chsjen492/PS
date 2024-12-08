a405ec43bd7afeb82641287ed2b3ecc859091c4f
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2024-12-08 21:58:50 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a, b, c;
+    cin>>a>>b>>c;
+    
+    cout<<stoi(a)+stoi(b)-stoi(c)<<'\n';
+    cout<<stoi(a+b)-stoi(c);
+}
