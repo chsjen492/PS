0d69132b50ddea5b94196ddba8d27f489eedf59e
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2025-01-11 23:40:52 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[31];
+    int n;
+    
+    for(int i=0;i<28;i++){
+        cin>>n;
+        arr[n]=1;
+    }
+    
+    for(int i=1;i<31;i++){
+        if(arr[i]!=1){
+            cout<<i<<'\n';
+        }
+    }
+}
