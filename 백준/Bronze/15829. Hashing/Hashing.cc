6d87ac5a21dba70f8ffb98085c03ea3c5f5f5ba5
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2025-02-04 00:47:00 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -4,15 +4,15 @@
 using namespace std;
 
 int main(){
-    int l, r = 31, m = 1234567891;
-    long long ret = 0, tmp = 1;
+    int l, r = 31;
+    long long ret = 0, tmp = 1,  m = 1234567891;
     string s;
     cin>>l;
     cin>>s;
     
     for(int i=0;i<l;i++){
-        ret+=((s[i]-'a'+1)*tmp)%m;
-        tmp = tmp*r%m;
+        ret=(ret+((s[i]-96)*tmp))%m;
+        tmp = (tmp*r)%m;
     }
     cout<<ret;
 }
