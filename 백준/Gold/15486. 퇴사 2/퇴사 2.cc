d7007ed0cd315b9388094e0712167621d8e1fcaf
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2025-04-14 21:15:22 +0900

    [Gold V] Title: 퇴사 2, Time: 184 ms, Memory: 19476 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int t[1500001]={};
-int p[1500001]={};
-int dp[1500001]={};
-    
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
     int n;
+    int t[1500001]={};
+    int p[1500001]={};
+    int dp[1500001]={};
+    
 
     int mx = 0;
     
