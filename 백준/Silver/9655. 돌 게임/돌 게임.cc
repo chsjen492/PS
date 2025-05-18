6c0cafd1301258ee99cad17a48460dd619dd51b9
Mina Lee <109032458+chsjen492@users.noreply.github.com>
2025-05-18 22:14:51 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+    int n;
+    cin>>n;
+    if(n%2==0) cout<<"CY";
+    else cout<<"SK";
+}
