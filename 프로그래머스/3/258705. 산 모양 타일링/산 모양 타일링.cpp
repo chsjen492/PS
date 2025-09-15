7b9afbae47226b428387bf1630166b79218fd085
Nina <109032458+chsjen492@users.noreply.github.com>
2025-09-15 22:29:27 +0900

    [level 3] Title: 산 모양 타일링, Time: 1.03 ms, Memory: 6.66 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n, vector<int> tops) {
+    int answer = 0;
+    int dp[100001];
+    dp[0] = 1;
+    dp[1] = 3 + tops[0];
+    
+    for(int i=2;i<=n;i++){
+        dp[i] = (dp[i-1] * (3 + tops[i-1]) - dp[i-2]) % 10007;
+        if (dp[i] < 0)
+            dp[i] += 10007;
+    }
+    
+    
+    answer = dp[n];
+    return answer;
+}
