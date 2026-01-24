731a5e54afa01718348880641a158f1f99202256
Nina <109032458+chsjen492@users.noreply.github.com>
2026-01-24 23:35:05 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin>>a>>b>>c;
+    
+    if(a==b && b==c){
+        cout<<10000+a*1000;
+    }
+    else if((a==b&&a!=c) || (a==c && a!=b) || (b==c && a!=b)) {
+        if(a==b || a==c) cout<<1000+a*100;
+        else cout<<1000+b*100;
+    }
+    else{
+        cout<<max(max(a, b), c)*100;
+    }
+}
