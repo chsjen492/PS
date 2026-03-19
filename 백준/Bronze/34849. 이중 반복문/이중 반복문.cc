0c40ea45931497ba624c97b4d9f832f8127380cf
Nina <109032458+ninaxlee@users.noreply.github.com>
2026-03-19 23:45:00 +0900

    [Bronze V] Title: 이중 반복문, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    if(n<=10000){
+        cout<<"Accepted";
+    }
+    else cout<<"Time limit exceeded";
+}
